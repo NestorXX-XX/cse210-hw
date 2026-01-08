bb86e5b3d4bed6801d7df37d902f70afca737f93
Nestor Gomez <nestorgomezartiles@gmail.com>
2026-01-08 17:55:03 +0000

    W01 Assignment: C# Programming Exercises

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What was your grade percentage? ");
+        int grade = int.Parse(Console.ReadLine());
+        char letter;
+
+        if (grade >= 90)
+        {
+            letter = 'A';
+        }
+        else if (grade >= 80)
+        {
+            letter = 'B';
+        }
+        else if (grade >= 70)
+        {
+            letter = 'C';
+        }
+        else if (grade >= 60)
+        {
+            letter = 'D';
+        }
+        else
+        {
+            letter = 'F';
+        }
+
+        Console.WriteLine($"Your grade is {letter}");
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations you passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("You failed the class. Keep working for next time");
+        }
     }
 }
--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int magic_number = randomGenerator.Next(1, 100);
+        int user_number;
+        int number_of_guesses = 0;
+        do
+        {
+            Console.Write("What is your guess? ");
+            user_number = int.Parse(Console.ReadLine());
+            number_of_guesses++;
+
+            if (user_number > magic_number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (user_number < magic_number)
+            {
+                Console.WriteLine("Higher");
+            }
+        } while (magic_number != user_number);
+
+        Console.WriteLine($"You guessed it! (It took you {number_of_guesses} guesses)");
+
+
     }
 }
