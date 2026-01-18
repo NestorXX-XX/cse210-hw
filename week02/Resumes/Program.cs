e263a010273b535a2a13c81ff2f116c29fff2fa3
Nestor Gomez <nestorgomezartiles@gmail.com>
2026-01-18 18:30:18 +0100

    W02 Learning Activity: Abstraction

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Apple";
+        Console.WriteLine(job1._company);
+        job1.DisplayJobDetails();
+
+        Job job2 = new Job();
+        job2._jobTitle = "Software Developer";
+        job2._company = "Amazon";
+        Console.WriteLine(job2._company);
+        job2.DisplayJobDetails();
+
+        Resume myResume = new Resume();
+        myResume._name = "Nestor Gomez Artiles";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume.DisplayResume();
+
     }
 }
